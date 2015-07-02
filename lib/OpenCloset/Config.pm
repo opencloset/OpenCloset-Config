f3756032c7999158a61851f22ba4d3e1d255640c
Keedi Kim <keedi.k@gmail.com>
2015-07-02 15:36:58 +0900

    Enable config root is selective

--- lib/OpenCloset/Config.pm
+++ lib/OpenCloset/Config.pm
@@ -10,13 +10,21 @@ use Path::Tiny;
 our $VERSION = '0.002';
 
 sub load {
-    my ( $conf_file, %default ) = @_;
+    my $conf_file = shift;
+    my %opt; %opt = %{ shift; } if ref $_[0] eq 'HASH';
+    my %default   = @_;
 
     $conf_file ||= $ENV{OPENCLOSET_CONFIG};
     die "cannot find config file\n" unless -e $conf_file;
     my $conf = eval path($conf_file)->slurp_utf8;
 
-    my %real_conf = ( %default, %$conf );
+    my %real_conf;
+    if ( $opt{root} ) {
+        %real_conf = ( %default, %{ $conf->{ $opt{root} } } );
+    }
+    else {
+        %real_conf = ( %default, %$conf );
+    }
 
     return \%real_conf;
 }
@@ -34,6 +42,18 @@ __END__
     my $conf              = OpenCloset::Config::load( 'app.conf' );
     my $conf_with_default = OpenCloset::Config::load(
         'app.conf',
+        key1 => 'default1',
+        key2 => 'default2',
+        key3 => 'default3',
+        key4 => 'default4',
+    );
+    my $conf_sub = OpenCloset::Config::load(
+        'app.conf',
+        { root => 'subkey' },
+    );
+    my $conf_sub_with_default = OpenCloset::Config::load(
+        'app.conf',
+        { root => 'subkey' },
         key1 => 'default1',
         key2 => 'default2',
         key3 => 'default3',
